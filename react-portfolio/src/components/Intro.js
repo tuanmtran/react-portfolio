ce58a24486338e6c689c66e228310d4e00af7fbb
Tuan (Tony) Tran <tmt003@ucsd.edu>
2021-07-13 14:43:56 -0700

    Started React App Development

---
+++ react-portfolio/src/components/Intro.js
@@ -0,0 +1,12 @@
+import React from "react"
+class Intro extends React.Component {
+  render() {
+    return (
+      <div className="Intro" class="p-6 items-center justify-center">
+        <h1 class="text-blue-400 font-extrabold">Hello World!</h1>
+        <p class="tracking-widest">This is my first React App.</p>
+    </div>
+    )
+  }
+}
+export default Intro
