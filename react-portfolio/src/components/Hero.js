a18dcfb16c54a940d7c20c3e457b200811b5186e
Tuan (Tony) Tran <tmt003@ucsd.edu>
2021-07-19 19:17:20 -0700

    Update Typer code

--- react-portfolio/src/components/Hero.js
+++ react-portfolio/src/components/Hero.js
@@ -1,20 +1,23 @@
 import React from "react"
-import CommandLineBox from "./Typer";
 
 class HeroSection extends React.Component {
   render() {
     return (
       
       <div class="font-sans text-4xl h-screen overflow-hidden relative flex flex-col justify-center items-center bg-gradient-to-t from-red-500 to-pink-400 text-white font-exo p-8">
-        <p class = ""> Hello there, I'm
-        </p> 
-        <CommandLineBox></CommandLineBox>
-        <p class="flex flex-col text-3xl"> Welcome to my website.
-        <a class="text-3xl resume" href="">Check out my resume</a>
-        <a class="text-3xl Links" href="https://github.com/tuanmtran"> GitHub </a>
-        <a class="text-3xl Links" href="https://www.linkedin.com/in/tuan-m-tran/"> Linkedin</a>
+        <p class="Whitespace-pre"> Hello there, I'm 
+          <a href="" class="typewrite" data-period="1000" data-type='[ " Tuan", " Tony", " excited to work with you!", " coffee dependent", " a non-stack dev"]'>
+              <span class="wrap"></span>
+          </a>
+
         </p>
-        
+        <div>
+          <p class="flex flex-col text-3xl"> Welcome to my website.
+            <a class="text-3xl resume" href="">Check out my resume</a>
+            <a class="text-3xl Links" href="https://github.com/tuanmtran"> GitHub </a>
+            <a class="text-3xl Links" href="https://www.linkedin.com/in/tuan-m-tran/"> Linkedin</a>
+          </p>
+        </div>
       </div>
     )
   }
@@ -23,7 +26,7 @@ var TxtType = function (el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
+  this.period = parseInt(period, 10) || 1000;
   this.txt = '';
   this.tick();
   this.isDeleting = false;
@@ -42,7 +45,7 @@ TxtType.prototype.tick = function () {
   this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
   var that = this;
-  var delta = 130 - Math.random() * 100;
+  var delta = 100 - Math.random() * 100;
 
   if (this.isDeleting) { delta /= 2; }
 
