3e19595a8a9dd14c3d725702e6439cf7aae331f6
Tuan (Tony) Tran <tmt003@ucsd.edu>
2021-11-17 15:46:45 -0800

    Update scaling for smaller screen

--- src/components/Current.js
+++ src/components/Current.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import CardPage from './Cards';
 class Current extends React.Component {
     render() {
         return (
             <div id="Ongoing" class="box-border h-screen w-sceen overflow-hidden">
                 <h1 class="justify-start relative inset-x-1/2 font-Roboto text-6xl font-semibold">Curr.</h1>
+                <CardPage></CardPage>
             </div>
         )
     }
