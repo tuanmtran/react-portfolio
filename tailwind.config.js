75d84c9f2cb3c95c659ffee304228f7ec153fde2
Tuan (Tony) Tran <tmt003@ucsd.edu>
2021-11-17 13:37:13 -0800

    pain

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {
       animation: {
